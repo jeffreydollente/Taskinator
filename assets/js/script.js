706754e64e7e9fe8a89e35948c75f951cf3eda0a
Jeffrey Dollente <jeffrey.dollente75@gmail.com>
2020-06-22 22:44:25 -0700

    commit task

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,9 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var pageContentEl = document.querySelector("#page-content");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
-  
+var tasks = [
+
+];
 var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
@@ -29,10 +31,17 @@ if (isEdit) {
 else {
   var taskDataObj = {
     name: taskNameInput,
-    type: taskTypeInput
+    type: taskTypeInput,
+    status: "to do"
   };
 
   createTaskEl(taskDataObj);
+  taskDataObj.id = taskIdCounter;
+
+  tasks.push(taskDataObj);
+
+  console.log(taskDataObj);
+  console.log(taskDataObj.status);
 }
   }
   var createTaskEl = function(taskDataObj) {
@@ -114,6 +123,19 @@ var taskButtonHandler = function(event) {
 var deleteTask = function(taskId) {
   var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
   taskSelected.remove();
+
+  // create new array to hold updated list of tasks
+  var updatedTaskArr = [];
+
+  // loop through current tasks
+  for (var i = 0; i < tasks.length; i++) {
+    // if tasks[i].id doesn't match the value of taskId, let's keep that task and push it into the new array
+    if (tasks[i].id !=== parseInt(taskId)) {
+      updatedTaskArr.push(tasks[i]);
+    }
+  }
+  // reassign tasks array to be the same as updatedTaskArr
+  tasks = updatedTaskArr;
 };
 
 var editTask = function(taskId) {
@@ -138,6 +160,14 @@ var completeEditTask = function(taskName, taskType, taskId) {
   // set new values
   taskSelected.querySelector("h3.task-name").textContent = taskName;
   taskSelected.querySelector("span.task-type").textContent = taskType;
+
+  // loop through tasks array and task object with new content
+  for (var i = 0; i < tasks.length; i++) {
+    if (tasks[i].id === parseInt(taskId)) {
+      tasks[i].name = taskName;
+      tasks[i].type = taskType;
+    }
+  };
   
   alert("Task Updated!");
   };
@@ -160,6 +190,13 @@ else if (statusValue === "in progress") {
 } 
 else if (statusValue === "completed") {
   tasksCompletedEl.appendChild(taskSelected);
+}
+// update task's in tasks array
+for (var i = 0; i , tasks.length; i++) {
+  if (tasks[i].id === parseInt(taskId)) {
+    tasks[i].status = statusValue;
+    console.log(tasks);
+  }
 }
   };
   var dragTaskHandler = function(event) {
@@ -186,6 +223,13 @@ else if (statusValue === "completed") {
       }
       dropZoneEl.appendChild(draggableElement);
       dropZoneEl.removeAttribute("style");
+      // loop through tasks array to find and update the updated task's status
+      for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(id)) {
+          tasks[i].status = statusSelectEl.value.toLowerCase();
+        }
+      }
+      console.log(tasks);
   };
 
   var dropZoneDragHandler = function(event) {
